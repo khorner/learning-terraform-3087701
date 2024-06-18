b5bec97379644dc6063ec2e18053ee899a6ac191
Kevin Horner <khorner@users.noreply.github.com>
2024-06-18 11:54:59 -0500

    adding alb

--- main.tf
+++ main.tf
@@ -57,3 +57,45 @@ module "blog_sg" {
   egress_rules        = ["all-all"]
   egress_cidr_blocks  = ["0.0.0.0/0"]
 }
+
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+  version = "9.9.0"
+  name    = "blog-alb"
+  
+  load_balancer_type = "application"
+
+  vpc_id  = module.blog_vpc.vpc_id
+  subnets = module.blog_vpc.public_subnets
+
+  # Security Group
+  security_groups = [module.blog_sg.security_group_id]
+
+  http_tcp_listeners = [
+    {
+      port = 80
+      protocol = "HTTP"
+      target_groups_index = 0
+    }
+  ]
+
+  target_groups = [
+    {
+      name_prefix      = "blog-"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+      targets = {
+        my_target = {
+          target_id = aws_instance.blog.id
+          port      = 80
+        }
+      }
+    }
+  ]
+
+  tags = {
+    Environment = "Development"
+    Project     = "Example"
+  }
+}

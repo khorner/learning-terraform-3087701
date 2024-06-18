30ecd7166f66879c1bb8a5ee237d17203ed62ff8
Kevin Horner <khorner@users.noreply.github.com>
2024-06-18 12:21:33 -0500

    autoscale

--- main.tf
+++ main.tf
@@ -33,16 +33,22 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami                    = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-  subnet_id              = module.blog_vpc.public_subnets[0]
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.5.2"
+  
+  name = "blog"
+  min_size = 1
+  max_size = 2
 
-  tags = {
-    Name = "Learning Terraform"
-  }
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns = module.blog_alb.target_group_arns
+  security_groups = [module.blog_sg.security_group_id]
+
+  image_id               = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
 }
+
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
@@ -58,7 +64,7 @@ module "blog_sg" {
   egress_cidr_blocks  = ["0.0.0.0/0"]
 }
 
-module "alb" {
+module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
   name    = "blog-alb"
